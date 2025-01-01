c54bdfff9ed958ff836aa4ef2aff46e1aad13062
USER <USER@DESKTOP-2R09BSK>
2025-01-01 13:42:04 +0330

    Create dbContext and Create first migration

---
+++ WebApiCRUD.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCRUD.API.Domain.Entity;
+
+namespace WebApiCRUD.API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Walk> Walks { get; set; }
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Difficulty> Difficulties { get; set; }
+    public DbSet<FileEntity> FileEntities { get; set; }
+
+}
