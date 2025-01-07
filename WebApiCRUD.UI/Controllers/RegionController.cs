846536641e1da10c4e7d767faab23dfa898b366f
USER <USER@DESKTOP-2R09BSK>
2025-01-07 18:40:56 +0330

    Add RegionControll And Model

---
+++ WebApiCRUD.UI/Controllers/RegionController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+using WebApiCRUD.UI.Models.Regions.DTOs;
+using WebApiCRUD.UI.Models.Regions.ViewModel;
+
+namespace WebApiCRUD.UI.Controllers
+{
+    public class RegionController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegionController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDTO> regions = new List<RegionDTO>();
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7228/Region");
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                regions.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDTO>>());
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View(regions);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7228/Region"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpRequestMessage.Content.ReadFromJsonAsync<RegionDTO>(); 
+
+            if(response is not null)
+            {
+                return RedirectToAction("Index", "Region");
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApiCRUD.UI/Models/Regions/ViewModel/AddRegionViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApiCRUD.UI.Models.Regions.ViewModel;
+public class AddRegionViewModel
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string? RegionImageUrl { get; set; }
+}
