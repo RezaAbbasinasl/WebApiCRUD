36499bae4dbad549df46e5583c86990d173d4c97
USER <USER@DESKTOP-2R09BSK>
2025-01-08 01:14:21 +0330

    Fixed Region View

--- WebApiCRUD.UI/Controllers/RegionController.cs
+++ WebApiCRUD.UI/Controllers/RegionController.cs
@@ -55,11 +55,88 @@ namespace WebApiCRUD.UI.Controllers
             var httpResponseMessage = await client.SendAsync(httpRequestMessage);
             httpResponseMessage.EnsureSuccessStatusCode();
 
-            var response = await httpRequestMessage.Content.ReadFromJsonAsync<RegionDTO>(); 
+            var response = await httpRequestMessage.Content.ReadFromJsonAsync<RegionDTO>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Region");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetFromJsonAsync<RegionDTO>($"https://localhost:7228/Region/{id.ToString()}");
+
+            if (response is not null)
+            {
+                return View(response);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(RegionDTO request)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7228/Region/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDTO>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Region");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetFromJsonAsync<RegionDTO>($"https://localhost:7228/Region/{id.ToString()}");
 
             if(response is not null)
             {
+                return View(response);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDTO request)
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"https://localhost:7228/Region/{request.Id}");
+                httpResponseMessage.EnsureSuccessStatusCode();
                 return RedirectToAction("Index", "Region");
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetFromJsonAsync<RegionDTO>($"https://localhost:7228/Region/{id.ToString()}");
+
+            if (response is not null)
+            {
+                return View(response);
             }
             return View();
         }
